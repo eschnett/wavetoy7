0932400b39ce2d73a3f40d955196d61829bb93ac
Erik Schnetter <schnetter@gmail.com>
2018-04-30 16:44:03 -0400

    Introduce MCategory, categories with flexible morphisms

---
+++ lib/Function.hs
@@ -0,0 +1,120 @@
+module Function
+    ( FnEqual(..), fnEqual, getFnEqual
+    , ObjKind
+    , MorKind, Morphism(..), Discretization(..), Ok, MId(..), MComp(..), (.:.)
+    , CatKind, MCategory(..)
+    , law_MCategory_comp_id_left
+    , law_MCategory_comp_id_right
+    , law_MCategory_comp_assoc
+    , type (-.>)(..)
+    ) where
+
+import Prelude
+import qualified Prelude as P
+
+import Data.Constraint
+import Data.Kind
+import GHC.Generics
+
+
+
+-- | Function equality
+data FnEqual a b = FnEqual (a -> b) (a -> b)
+fnEqual :: (Ok m a b, Ok n a b) => a `m` b -> a `n` b -> FnEqual a b
+fnEqual fx gx = FnEqual (chase fx) (chase gx)
+getFnEqual :: FnEqual a b -> a -> (b, b)
+getFnEqual (FnEqual fx gx) x = (fx x, gx x)
+
+
+
+-- | Objects
+type ObjKind = Type -> Constraint
+
+
+
+-- | Morphism
+type MorKind = Type -> Type -> Type
+class MCategory (Cat m) => Morphism (m :: MorKind) where
+    type Cat m :: CatKind
+    chase :: (Obj (Cat m) a, Obj (Cat m) b) => a `m` b -> a -> b
+
+class Morphism m => Discretization m where
+    discretize :: (Obj (Cat m) a, Obj (Cat m) b) => (a -> b) -> a `m` b
+
+type Ok m a b = (Morphism m, Obj (Cat m) a, Obj (Cat m) b)
+
+data MId (k :: CatKind) a b where
+    MId :: MId k a a
+deriving instance Eq (MId k a b)
+deriving instance Ord (MId k a b)
+deriving instance Show (MId k a b)
+
+instance MCategory k => Morphism (MId k) where
+    type Cat (MId k) = k
+    chase MId = P.id
+
+data MComp n m b a c = MComp (n b c) (m a b)
+                       deriving (Eq, Ord, Read, Show, Generic)
+infixr 9 .:.
+(.:.) :: n b c -> m a b -> MComp n m b a c
+(.:.) = MComp
+
+instance (Morphism m, Morphism n, Cat m ~ Cat n, Obj (Cat m) b)
+        => Morphism (MComp n m b) where
+    type Cat (MComp n m b) = Cat m
+    chase (MComp g f) = chase g . chase f
+
+
+
+-- | MCategory
+type CatKind = Type -> Constraint
+class MCategory (k :: CatKind) where
+    type Obj k :: ObjKind
+    type Obj k = k
+
+
+
+-- id . f = f
+law_MCategory_comp_id_left :: Ok m a b => a `m` b -> FnEqual a b
+law_MCategory_comp_id_left f = (MId .:. f) `fnEqual` f
+
+-- f . id = f
+law_MCategory_comp_id_right :: Ok m a b => a `m` b -> FnEqual a b
+law_MCategory_comp_id_right f = (f .:. MId) `fnEqual` f
+
+-- (h . g) . f = h . (g . f)
+law_MCategory_comp_assoc :: ( Cat m ~ Cat n, Cat n ~ Cat o
+                           , Ok m a b, Ok n b c, Ok o c d
+                           ) => c `o` d -> b `n` c -> a `m` b -> FnEqual a d
+law_MCategory_comp_assoc h g f = ((h .:. g) .:. f) `fnEqual` (h .:. (g .:. f))
+
+
+
+
+-- | Hask
+class Hask a
+instance Hask a
+
+instance MCategory Hask
+
+instance Morphism (->) where
+    type Cat (->) = Hask
+    chase = id
+
+instance Discretization (->) where
+    discretize = id
+
+
+
+-- | Num
+instance MCategory Num
+
+newtype (-.>) a b = NFun { getNFun :: a -> b }
+    deriving Generic
+
+instance Morphism (-.>) where
+    type Cat (-.>) = Num
+    chase = getNFun
+
+instance Discretization (-.>) where
+    discretize = NFun
---
+++ test/FunctionSpec.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module FunctionSpec where
+
+import Test.QuickCheck
+import Test.QuickCheck.Poly
+
+import Function
+
+
+
+prop_Hask_MCategory_comp_id_left :: Fun A B -> A -> Property
+prop_Hask_MCategory_comp_id_left (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_id_left f) x)
+
+prop_Hask_MCategory_comp_id_right :: Fun A B -> A -> Property
+prop_Hask_MCategory_comp_id_right (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_id_right f) x)
+
+prop_Hask_MCategory_comp_assoc :: Fun A C -> Fun B A -> Fun A B -> A -> Property
+prop_Hask_MCategory_comp_assoc (Fn h) (Fn g) (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_assoc h g f) x)
+
+
+
+newtype NA = NA Integer
+    deriving (Eq, Ord, Read, Show, Num, Arbitrary, CoArbitrary)
+instance Function NA where
+    function = functionMap (\(NA x) -> x) NA
+newtype NB = NB Integer
+    deriving (Eq, Ord, Read, Show, Num, Arbitrary, CoArbitrary)
+instance Function NB where
+    function = functionMap (\(NB x) -> x) NB
+newtype NC = NC Integer
+    deriving (Eq, Ord, Read, Show, Num, Arbitrary, CoArbitrary)
+instance Function NC where
+    function = functionMap (\(NC x) -> x) NC
+
+
+
+prop_Num_MCategory_comp_id_left :: Fun NA NB -> NA -> Property
+prop_Num_MCategory_comp_id_left (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_id_left (NFun f)) x)
+
+prop_Num_MCategory_comp_id_right :: Fun NA NB -> NA -> Property
+prop_Num_MCategory_comp_id_right (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_id_right (NFun f)) x)
+
+prop_Num_MCategory_comp_assoc ::
+    Fun NA NC -> Fun NB NA -> Fun NA NB -> NA -> Property
+prop_Num_MCategory_comp_assoc (Fn h) (Fn g) (Fn f) x =
+    uncurry (===) (getFnEqual (law_MCategory_comp_assoc
+                                      (NFun h) (NFun g) (NFun f)) x)
